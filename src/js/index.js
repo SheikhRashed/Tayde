2879ff1631b59ecb6a4f3e1e69deca8417912b89
SheikhRashed <devrashed06@gmail.com>
2022-12-01 02:56:28 +0600

    update/banner: created

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,8 @@ year.innerHTML = new Date().getFullYear();
 // Sidebar 
 document.querySelector('#menubar').addEventListener('click', function () {
   document.querySelector('.sidebar').classList.toggle('show');
-  document.querySelector('body').style.overflow = 'hidden';
+  document.querySelector('body').classList.toggle('overflow-hidden')
+
 })
 
 // outside click 
@@ -20,3 +21,41 @@ document.querySelector('.sidebar').addEventListener('click', function (event) {
     document.querySelector('body').style.overflow = 'auto';
   }
 })
+
+
+// countdown js 
+
+$("#time")
+  .countdown("2022/12/31")
+  .on("update.countdown", function (event) {
+    var $this = $(this).html(
+      event.strftime(
+        "" +
+        ` 
+        <div>
+            <span>%D</span>
+            <p>Days</p>
+        </div>
+      ` +
+        `
+        <div>
+          <span>%H</span>
+          <p>Hours</p>
+        </div>
+        ` +
+        `
+        <div>
+          <span>%M</span>
+          <p>Minutes</p>
+        </div>
+        `
+        +
+        `
+          <div>
+            <span>%S</span>
+            <p>Seconds</p>
+          </div>
+        `
+      )
+    );
+  });
