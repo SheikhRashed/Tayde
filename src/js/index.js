16a6c82fd4d3c2dbcada7aac02fa605588f38e5a
SheikhRashed <devrashed06@gmail.com>
2022-11-30 22:56:13 +0600

    update/header: done

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,19 @@
 const year = document.querySelector('#year')
 year.innerHTML = new Date().getFullYear();
 
-console.log(year)
 
 
+// Sidebar 
+document.querySelector('#menubar').addEventListener('click', function () {
+  document.querySelector('.sidebar').classList.toggle('show');
+  document.querySelector('body').style.overflow = 'hidden';
+})
 
+// outside click 
+document.querySelector('.sidebar').addEventListener('click', function (event) {
+
+  if (event.target.matches('#menubar') || !event.target.closest('.sidebar-content')) {
+    document.querySelector('.sidebar').classList.remove('show')
+    document.querySelector('body').style.overflow = 'auto';
+  }
+})
